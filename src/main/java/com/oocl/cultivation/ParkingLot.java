016d998d18852e73b78a2d9f24a85e852ae17288
littleDrive <ZERO.OU@OOCL.COM>
2020-07-24 15:44:19 +0800

    [parking-lot] [zero] feature: implement should_return_car_when_fetch_car_given_ticket

--- src/main/java/com/oocl/cultivation/ParkingLot.java
+++ src/main/java/com/oocl/cultivation/ParkingLot.java
@@ -1,12 +1,17 @@
 package com.oocl.cultivation;
 
+import java.util.HashMap;
+
 public class ParkingLot {
+    HashMap<Ticket, Car> parkingRooms = new HashMap<>();
 
     public Ticket park(Car car) {
-        return new Ticket();
+        Ticket ticket = new Ticket();
+        parkingRooms.put(ticket, car);
+        return ticket;
     }
 
     public Car fetch(Ticket ticket) {
-        return null;
+        return parkingRooms.get(ticket);
     }
 }

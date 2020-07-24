552f6f996098b74d1b949a773b1709bf96cd9ec1
littleDrive <ZERO.OU@OOCL.COM>
2020-07-24 15:41:49 +0800

    [parking-lot] [zero] feature: add should_return_car_when_fetch_car_given_ticket test

--- src/test/java/com/oocl/cultivation/test/ParkingLotTest.java
+++ src/test/java/com/oocl/cultivation/test/ParkingLotTest.java
@@ -20,4 +20,19 @@ public class ParkingLotTest {
         //then
         Assertions.assertNotNull(ticket);
     }
+
+    @Test
+    void should_return_car_when_fetch_car_given_ticket() {
+        //given
+        Car parking_car = new Car();
+        ParkingLot parkingLot = new ParkingLot();
+        Ticket ticket = parkingLot.park(parking_car);
+
+        //when
+        Car fetch_car = parkingLot.fetch(ticket);
+
+        //then
+        Assertions.assertEquals(parking_car, fetch_car);
+
+    }
 }

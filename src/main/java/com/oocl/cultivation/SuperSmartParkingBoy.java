b28490a0d71e2a1434de9a62b0adce364cb86fc6
littleDrive <2682155146@qq.com>
2020-07-26 18:56:54 +0800

    [parking_lot] [zero] feature: implement should_park_cars_to_the_parking_lot_which_has_a_larger_available_position_rate_when_park_car

--- src/main/java/com/oocl/cultivation/SuperSmartParkingBoy.java
+++ src/main/java/com/oocl/cultivation/SuperSmartParkingBoy.java
@@ -12,5 +12,25 @@ public class SuperSmartParkingBoy extends ParkingBoy {
         this.parkingLots = parkingLots;
     }
 
+    @Override
+    public Ticket park(Car car) {
+        Ticket ticket = null;
+        double largerAvailablePosition = 0;
+        ParkingLot largerAvailablePositionParkingLot = parkingLots.get(0);
+        for (ParkingLot parkingLot : parkingLots) {
+            double currentParkingLotAvailablePosition = (double) (parkingLot.getCapicity() - parkingLot.parkingRooms.size()) / parkingLot.getCapicity() ;
+            if (largerAvailablePosition < currentParkingLotAvailablePosition) {
+                largerAvailablePosition = currentParkingLotAvailablePosition;
+                largerAvailablePositionParkingLot = parkingLot;
+            }
+        }
+        if (largerAvailablePosition > 0) {
+            return largerAvailablePositionParkingLot.park(car);
+
+        }
+
+        errorMassage = "Not enough position.";
+        return ticket;
+    }
 
 }

7a1c322acca416a267da8b831624bb0dec4205a8
littleDrive <2682155146@qq.com>
2020-07-26 18:45:17 +0800

    [parking_lot] [zero] feature: add should_park_cars_to_the_parking_lot_which_has_a_larger_available_position_rate_when_park_car test

---
+++ src/main/java/com/oocl/cultivation/SuperSmartParkingBoy.java
@@ -0,0 +1,16 @@
+package com.oocl.cultivation;
+
+import java.util.List;
+
+/**
+ * @author ozm
+ * @date 2020/7/26 - 18:42
+ */
+public class SuperSmartParkingBoy extends ParkingBoy {
+
+    public SuperSmartParkingBoy(List<ParkingLot> parkingLots) {
+        this.parkingLots = parkingLots;
+    }
+
+
+}
---
+++ src/test/java/com/oocl/cultivation/test/SuperSmartParkingBoyTest.java
@@ -0,0 +1,41 @@
+package com.oocl.cultivation.test;
+
+import com.oocl.cultivation.*;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author ozm
+ * @date 2020/7/26 - 18:17
+ */
+public class SuperSmartParkingBoyTest {
+    @Test
+    void should_park_cars_to_the_parking_lot_which_has_a_larger_available_position_rate_when_park_car() {
+        //given
+        List<ParkingLot> parkingLots = new ArrayList<>();
+        parkingLots.add(new ParkingLot(5, 1));
+        for (int i = 0; i < 3; i++) {
+            parkingLots.get(0).park(new Car());
+        }
+        parkingLots.add(new ParkingLot(10, 2));
+        for (int i = 0; i < 4; i++) {
+            parkingLots.get(1).park(new Car());
+        }
+        parkingLots.add(new ParkingLot(15, 3));
+        for (int i = 0; i < 8; i++) {
+            parkingLots.get(2).park(new Car());
+        }
+
+        ParkingBoy superSmarkParkingBoy = new SuperSmartParkingBoy(parkingLots);
+        Car car = new Car();
+
+        //when
+        Ticket ticket = superSmarkParkingBoy.park(car);
+
+        //then
+        Assertions.assertEquals(2, ticket.getParkingLotId());
+    }
+}

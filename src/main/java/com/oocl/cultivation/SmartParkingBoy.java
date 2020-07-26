98363fb109b6f9ce25529f606a97aed1cdba6730
littleDrive <2682155146@qq.com>
2020-07-26 18:16:09 +0800

    [parking_lot] [zero] feature: implement should_park_cars_to_the_parking_lot_which_contains_more_empty_positions_when_parking_cars

--- src/main/java/com/oocl/cultivation/SmartParkingBoy.java
+++ src/main/java/com/oocl/cultivation/SmartParkingBoy.java
@@ -12,4 +12,25 @@ public class SmartParkingBoy extends ParkingBoy {
     public SmartParkingBoy(List<ParkingLot> parkingLots) {
         this.parkingLots = parkingLots;
     }
+
+    @Override
+    public Ticket park(Car car) {
+        Ticket ticket = null;
+        int maxEmptyPosition = 0;
+        ParkingLot maxEmptyPositionParkingLot = parkingLots.get(0);
+        for (ParkingLot parkingLot : parkingLots) {
+            int currentParkingLotEmptyPosition = parkingLot.getCapicity() - parkingLot.parkingRooms.size();
+            maxEmptyPosition = maxEmptyPosition > currentParkingLotEmptyPosition ? maxEmptyPosition : currentParkingLotEmptyPosition;
+            if (maxEmptyPosition == currentParkingLotEmptyPosition) {
+                maxEmptyPositionParkingLot = parkingLot;
+            }
+        }
+        if (maxEmptyPosition > 0) {
+            return maxEmptyPositionParkingLot.park(car);
+
+        }
+
+        errorMassage = "Not enough position.";
+        return ticket;
+    }
 }

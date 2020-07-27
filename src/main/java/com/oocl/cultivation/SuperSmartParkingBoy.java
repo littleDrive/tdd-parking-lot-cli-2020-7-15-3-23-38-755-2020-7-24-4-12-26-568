8ea984be6ad563733bcf63f850e8b1e38fef8563
littleDrive <2682155146@qq.com>
2020-07-27 22:30:13 +0800

    [story4] [zero] refactor: convert Not enough position. to const

--- src/main/java/com/oocl/cultivation/SuperSmartParkingBoy.java
+++ src/main/java/com/oocl/cultivation/SuperSmartParkingBoy.java
@@ -8,6 +8,8 @@ import java.util.List;
  */
 public class SuperSmartParkingBoy extends ParkingBoy {
 
+    private static final String NOT_ENOUGH_POSITION = "Not enough position.";
+
     public SuperSmartParkingBoy(List<ParkingLot> parkingLots) {
         super(parkingLots);
     }
@@ -29,7 +31,7 @@ public class SuperSmartParkingBoy extends ParkingBoy {
 
         }
 
-        errorMassage = "Not enough position.";
+        errorMassage = NOT_ENOUGH_POSITION;
         return ticket;
     }
 

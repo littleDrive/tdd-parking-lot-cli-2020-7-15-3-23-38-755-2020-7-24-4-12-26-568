cc18767a22cdce24b740a95f589b450f695f2413
littleDrive <ZERO.OU@OOCL.COM>
2020-07-27 17:06:29 +0800

    [parking lot] [zero] refactor: delete import org.junit.jupiter.api.Assertions;

--- src/test/java/com/oocl/cultivation/test/ParkingLotTest.java
+++ src/test/java/com/oocl/cultivation/test/ParkingLotTest.java
@@ -3,7 +3,6 @@ package com.oocl.cultivation.test;
 import com.oocl.cultivation.Car;
 import com.oocl.cultivation.ParkingLot;
 import com.oocl.cultivation.Ticket;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
--- src/test/java/com/oocl/cultivation/test/SuperSmartParkingBoyTest.java
+++ src/test/java/com/oocl/cultivation/test/SuperSmartParkingBoyTest.java
@@ -1,7 +1,6 @@
 package com.oocl.cultivation.test;
 
 import com.oocl.cultivation.*;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;

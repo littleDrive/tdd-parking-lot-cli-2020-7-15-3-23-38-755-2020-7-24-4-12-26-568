96c76ce439015f2e0566abfe6eba7c91eb07b402
littleDrive <2682155146@qq.com>
2020-08-02 15:19:58 +0800

    [story6][zero] feature: implement should_return_error_massage_when_parking_boy_can_not_parking_car_given_parking_manager

--- src/main/java/com/oocl/cultivation/ParkingManager.java
+++ src/main/java/com/oocl/cultivation/ParkingManager.java
@@ -5,6 +5,10 @@ import java.util.List;
 
 public class ParkingManager extends ParkingBoy{
 
+    private static final String NOT_ENOUGH_POSITION = "Not enough position.";
+    public static final String UNRECOGNIZED_PARKING_TICKET = "Unrecognized parking ticket.";
+    public static final String PLEASE_PROVIDE_YOUR_PARKING_TICKET = "Please provide your parking ticket.";
+
     List<ParkingBoy> parkingBoys;
 
     public ParkingManager(List<ParkingLot> parkingLots) {
@@ -13,7 +17,11 @@ public class ParkingManager extends ParkingBoy{
     }
 
     public Ticket park(ParkingBoy parkingBoy, Car car) {
-        return parkingBoy.park(car);
+        Ticket ticket = parkingBoy.park(car);
+        if (ticket == null) {
+            errorMassage = NOT_ENOUGH_POSITION;
+        }
+        return ticket;
     }
 
     public void addParkingBoye(ParkingBoy parkingBoy) {
